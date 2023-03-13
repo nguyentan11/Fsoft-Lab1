4baa11a2c9e7836f3fb67be4e3d9c79b4be21db1
ntan1107@gmail.com <nguyentan11>
2023-03-13 09:54:25 +0700

    update TF

--- newrelic/windows-vm-main.tf
+++ newrelic/windows-vm-main.tf
@@ -18,8 +18,9 @@ Powershell -ExecutionPolicy Unrestricted -File ConfigureRemotingForAnsible.ps1;
 New-LocalUser -Name "${data.aws_ssm_parameter.win-user.value}" -Password (ConvertTo-SecureString -AsPlainText "${data.aws_ssm_parameter.win-pass.value}" -Force);
 Add-LocalGroupMember -Group "Administrators" -Member "${data.aws_ssm_parameter.win-user.value}";
 
-winrm set winrm/config/service/auth '@{Basic="true"}';
 winrm set winrm/config/service/auth '@{CredSSP="true"}';
+winrm set winrm/config/service/auth '@{Basic="true"}';
+winrm set winrm/config/client/auth '@{Basic="true"}';
 winrm set winrm/config/service '@{AllowUnencrypted="true"}';
 
 </powershell>
